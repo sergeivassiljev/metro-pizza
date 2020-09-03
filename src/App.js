0093e38bd12dfbf32ac9853df86a4cae4db0ae28
metronomepy <sergei.vassiljev@tkvg.ee>
2020-09-03 13:33:25 +0300

    local server db

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import { Home, Cart } from './pages';
 import { Route } from 'react-router-dom';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
+  console.log(pizzas);
+
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" render={() => <Home items={pizzas} />} />
           <Route exact path="/cart" component={Cart} />
         </div>
       </div>
